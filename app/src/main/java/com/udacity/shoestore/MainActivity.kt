f36f5c46398292ea7111d7f7c9de35117561f002
Ramiro Torrejón <ramijtc@gmail.com>
2021-02-11 22:48:20 -0500

    Initial commit

---
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+
+
+    }
+}
